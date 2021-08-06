df28ac375e4dc8f43713f376c1621786541f9faf
Carlos Frederico Magalhães <cfcmagalhaes@yahoo.com.br>
2021-08-06 17:09:16 -0300

    Implementação do JWT

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -10,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -18,6 +21,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 
     @Autowired
     private AutenticacaoService autenticacaoService;
+    
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager( ) throws Exception
+    {
+        return super.authenticationManager( );
+    }
 
     // Configurações de autenticação
     @Override
@@ -36,7 +46,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 		.antMatchers( HttpMethod.POST, "/auth" ).permitAll( )
 		.anyRequest( ).authenticated( )
 		.and( ).csrf( ).disable( )
-		.sessionManagement( ).sessionCreationPolicy( SessionCreationPolicy.STATELESS );
+		.sessionManagement( ).sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+		.and( ).addFilterBefore( new AutenticacaoViaTokenFilter( ), UsernamePasswordAuthenticationFilter.class );
 //		.and( ).formLogin( );
     }
 
